215604126efb399c77ee6b0f0e7ad296088a0cb3
JoalG <53712282+JoalG@users.noreply.github.com>
2020-03-05 22:58:01 -0600

    Lector y DataBase
    Se creo el script que lee el archivo .txt con el formato deseado , los datos se almacenan en una clase llamada DataBase

---
+++ NonogramSolver_AA/Assets/DataBase.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataBase : MonoBehaviour
+{
+    private int rows;
+    private int columns;
+    private int[][] rowsArray;
+    private int[][] columnsArray;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int Columns
+    {
+        get => columns;
+        set => columns = value;
+    }
+
+    public int Rows
+    {
+        get => rows;
+        set => rows = value;
+    }
+
+    public int[][] ColumnsArray
+    {
+        get => columnsArray;
+        set => columnsArray = value;
+    }
+
+    public int[][] RowsArray
+    {
+        get => rowsArray;
+        set => rowsArray = value;
+    }
+
+
+    
+    
+    
+}
---
+++ NonogramSolver_AA/Assets/Lector.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class Lector : MonoBehaviour
+{
+    private string file = "";
+    public DataBase dataBase;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        readFile("");
+
+    }
+
+    public void readFile(string name)
+    {
+        using (StreamReader fr = File.OpenText("Nonogram.txt"))
+        {
+            file = fr.ReadLine();
+            setCounter(file);
+
+            file = fr.ReadLine();
+            print("####################Array de filas ####################");
+            for (int i = 0; i < dataBase.Rows; i++)
+            {
+                file =  fr.ReadLine();
+                print(file);
+                dataBase.RowsArray[i] = obtainArray(file);
+                
+                for (int j = 0; j < dataBase.RowsArray[i].Length; j++)
+                {
+                    print(dataBase.RowsArray[i][j]);
+                }
+            }
+            
+            file = fr.ReadLine();
+            print("####################Array de Columnas ####################");
+            for (int i = 0; i < dataBase.Rows; i++)
+            {
+                file =  fr.ReadLine();
+                print(file);
+                dataBase.ColumnsArray[i] = obtainArray(file);
+                
+                for (int j = 0; j < dataBase.ColumnsArray[i].Length; j++)
+                {
+                    print(dataBase.ColumnsArray[i][j]);
+                }
+            }
+            
+        }
+    }
+
+    public void setCounter(string line)
+    {
+        string value = "";
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (line[i] != ',')
+            {
+                value += line[i];
+            }
+            else
+            {
+                dataBase.Rows = Convert.ToInt32(value);
+                dataBase.RowsArray = new int[Convert.ToInt32(value)][];
+                i++;
+                value = "";
+            }
+        }
+        dataBase.Columns = Convert.ToInt32(value);
+        dataBase.ColumnsArray = new int[Convert.ToInt32(value)][];
+        
+    }
+
+    public int[] obtainArray(string line)
+    {
+        int[] res = new int[cantValores(line)];
+        string value = "";
+        int pos = 0; 
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (line[i] != ',')
+            {
+                value += line[i];
+            }
+            else
+            {
+                res[pos]= Convert.ToInt32(value);
+                i++;
+                value = "";
+                pos++;
+            }
+        }
+        res[pos]= Convert.ToInt32(value);
+        
+
+        return res;
+    }
+
+    public int cantValores(string line)
+    {
+        string value = "";
+        int res = 1;
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (line[i] == ',')
+            {
+                res++;
+            }
+
+        }
+
+        return res;
+    }
+
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
